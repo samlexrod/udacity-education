9644c01c9a7e413d693750a363417b0c88fc4fc4
Samuel Rodriguez <31121205+samlexrod@users.noreply.github.com>
2025-01-19 18:50:57 -0500

    Remove unnecessary directory creation and change in dogImagesSetup.sh

--- projects/operationalizing/dogImagesSetup.sh
+++ projects/operationalizing/dogImagesSetup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Create folder and download dog dataset
-mkdir dogImages
-cd dogImages
 wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/dogImages.zip
-unzip dogImages.zip -d .
+unzip dogImages.zip
 rm -f dogImages.zip
 
 echo "Dog dataset downloaded and unzipped"
