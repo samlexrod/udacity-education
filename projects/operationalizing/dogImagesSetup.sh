85a1b1456753273cdde88028bb0630f5c7da3669
Samuel Rodriguez <31121205+samlexrod@users.noreply.github.com>
2025-01-19 18:37:31 -0500

    Unzip dogImages.zip directly into the current directory

--- projects/operationalizing/dogImagesSetup.sh
+++ projects/operationalizing/dogImagesSetup.sh
@@ -4,7 +4,7 @@
 mkdir dogImages
 cd dogImages
 wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/dogImages.zip
-unzip dogImages.zip
+unzip dogImages.zip -d .
 rm -f dogImages.zip
 
 echo "Dog dataset downloaded and unzipped"
